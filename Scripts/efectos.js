8bb229e38d3705a8b7531103b100c138ec747aca
luisfermata <luisfermata@icloud.com>
2018-09-01 18:11:41 -0600

    efectos de font, color y tamanio

--- Scripts/efectos.js
+++ Scripts/efectos.js
@@ -25,3 +25,66 @@ function cambiar_tema(){
             $(".cambio_tema").addClass("green");
     }
 }
+
+function cambiar_color_letras(){
+    
+    var color;
+
+    if($("p").first().hasClass("black-text"))
+        color = 1;
+    
+    if($("p").first().hasClass("green-text"))
+        color = 2;
+
+    if($("p").first().hasClass("blue-text"))
+        color = 3;
+
+    if($("p").first().hasClass("yellow-text"))
+        color = 4;
+
+    switch(color) {
+        case 1:
+            $("p").removeClass("black-text");
+            $("p").addClass("green-text");
+            break;
+        case 2:
+            $("p").removeClass("green-text");
+            $("p").addClass("blue-text");
+            break;
+        case 3:
+            $("p").removeClass("blue-text");
+            $("p").addClass("yellow-text");
+            break;
+        default:
+            $("p").removeClass("yellow-text");
+            $("p").addClass("black-text");
+    }
+}
+
+function cambiar_font_size(){
+    
+    var color;
+
+    if($("p").first().hasClass("text1"))
+        color = 1;
+    
+    if($("p").first().hasClass("text2"))
+        color = 2;
+
+    if($("p").first().hasClass("text3"))
+        color = 3;
+
+    switch(color) {
+        case 1:
+            $("p").removeClass("text1t");
+            $("p").addClass("text2");
+            break;
+        case 2:
+            $("p").removeClass("text2");
+            $("p").addClass("text3");
+            break;
+        default:
+            $("p").removeClass("text3");
+            $("p").addClass("text1");
+    }
+}
